af164c64b781639be194cca88efd13ac1bede9c9
ultragrace <udechukwuakachukwu.com>
2022-05-09 21:07:42 +0100

    create the updateScreen function that update the what button is been clicked also evaluate the calculations

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
+import React, { useState } from "react";
 
 function App() {
+  // creating our status
+  const [screen, setScreen] = useState("");
+  const [result, setResult] = useState("");
 
+  const operators = ["/", "*", "+", ".", "-"];
+
+  const updateScreen = (value) => {
+    // condition to prevent an oprator to repeat twis repectively
+    if (
+      (operators.includes(value) && screen === "") ||
+      (operators.includes(value) && operators.includes(screen.slice(-1)))
+    ) {
+      return
+    }
+    setScreen(screen + value);
+
+    // when the operator is to clicked
+    if (!operators.includes(value)) {
+      // console.log(screen);
+      setResult(eval(screen + value).toString());
+    }
+  };
+
+  // function to generate buttons from 1-9
   const createButtons = () => {
     let digits = [];
-    let numBtn = 10
+    let numBtn = 10;
     for (let i = 1; i < numBtn; i++) {
       digits.push(
-        <button key={i}>{i}</button>
-      )
+        <button onClick={() => updateScreen(i)} key={i}>
+          {i}
+        </button>
+      );
     }
     return digits;
-  }
+  };
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span>0
+          {result ? <span>({result})</span>: ""}
+          &nbsp;
+          {screen || "0"}
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateScreen("/")}>/</button>
+          <button onClick={() => updateScreen("*")}>*</button>
+          <button onClick={() => updateScreen("+")}>+</button>
+          <button onClick={() => updateScreen("-")}>-</button>
           <button>DEL</button>
         </div>
         <div className="digits">
           {createButtons()}
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => updateScreen("0")}>0</button>
+          <button onClick={() => updateScreen(".")}>.</button>
           <button>=</button>
         </div>
       </div>
