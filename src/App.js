9b4508276e22296e59f4b900d19442f36a665628
ultragrace <udechukwuakachukwu.com>
2022-05-09 20:00:03 +0100

    Done with html structuer of the app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+function App() {
+
+  const createButtons = () => {
+    let digits = [];
+    let numBtn = 10
+    for (let i = 0; i < numBtn; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          (0)0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createButtons()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
